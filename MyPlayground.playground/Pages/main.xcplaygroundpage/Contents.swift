18799ec6f3dae8b26b7ea35f3c74c89c5400feca
jacqschweiger <jacqschweiger@gmail.com>
2016-08-30 21:55:26 -0400

    Done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -16,85 +16,105 @@
 /*: Question 1
 ### 1. Add two Doubles
 */
-// write your code here
+5.2 + 5.2
+
 
 
 /*: Question 2
 ### 2. Add an Int to a Double
 */
-// write your code here
+Double(4) + 5.2
 
 
 /*: Question 3
 ### 3. Compare two Ints for equality
 */
-// write your code here
+3 == 4
 
 
 /*: Question 4
 ### 4. Compare two Doubles for equality
 */
-// write your code here
+3.4 == 5.4
 
 
 /*: Question 5
 ### 5. Compare an Int and a Double equality
 */
-// write your code here
+Int(5.4) > 3
 
 
 /*: Question 6
 ### 6. Compare 3 and 3.5 for equality (NB what do you have to be mindful of in this case?)
 */
-// write your code here
+Double(3) == 3.0
 
 
 /*: Question 7
 ### 7. Compare 3 and 3.5 for equality and 6.1 and 7 for inequality
 */
-// write your code here
+Double(3) == 3.5 && 6.1 != Double(7)
 
 
 /*: Question 8
 ### 8. Write code expressing "if a less than b or x is greater than or equal to y"
 */
-// write your code here
+var a = 5
+var b = 6
+var x = 8
+var y = 7
+
+if ((a < b) || (x >= y)) {
+    print(true)
+}
 
 
 /*: Question 9
 ### 9. Write a function which returns the sum of three Ints
 */
-// write your code here
+func sumThreeInts(a:Int, b:Int, c:Int) -> Int {
+    return(a + b + c)
+}
 
+sumThreeInts(3, b:2, c:4)
 
 /*: Question 10
 ### 10. Write a function average_i which returns the average of three Ints as an Int
 */
-// write your code here
 
 
+
+func average_i(a:Int, b:Int, c:Int) -> Int {
+    return((a+b+c)/3)
+}
+
+average_i(9, b:6, c:3)
 /*: Question 11
 ### 11. Use the return value of the function written for Question 10 in a boolean expression
 */
-// write your code here
 
+average_i(9, b:6, c:3) == 6
 
 /*: Question 12
 ### 12. Write a function average_f which returns the average of three Ints as a Float
 */
-// write your code here
+func average_f(a:Int, b:Int, c:Int) -> Float {
+    return((Float(a)+Float(b)+Float(c))/3)
+}
+
+average_f(9, b:6, c:3)
 
 
 /*: Question 13
 ### 13. Call average_f with values 1, 3, and 5 and test if the result equals 3.0
 */
-// write your code here
+average_f(1, b:3, c:5) == 3.0
 
 
 /*: Question 14
 ### 14. Call average_i with values 1, 3, and 5 and test if the result is both greater than 1 and less than 5.
 */
-// write your code here
+average_i(1, b:3, c:5) > 1 && average_i(1, b:3, c:5) < 5
 
 
 
